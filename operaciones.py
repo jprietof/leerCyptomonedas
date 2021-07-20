0b48a0c86f0884c5a44720151d31271f85f07c99
jprietof <jprietof@outlook.es>
2021-07-20 00:45:06 -0500

    Se realizaron cambios a los archivos

--- operaciones.py
+++ operaciones.py
@@ -2,29 +2,14 @@ class Archivos(object):
     def __init__(self, ruta, permisos):
         self.ruta = ruta
         self.permisos = permisos
-
-    def abrir_archivo(self):
-        archivo = open(self.ruta, self.permisos, encoding="utf8")
-        return archivo
+        self.archivo = open(self.ruta, self.permisos, encoding="utf8")
 
     def leer_archivo(self):
-        archivo = open(self.ruta, self.permisos, encoding="utf8")
-        ver_doc = archivo.readlines()
+        ver_doc = self.archivo.read()
         return ver_doc
 
     def escribir_archivo(self, texto):
-        archivo = open(self.ruta, self.permisos, encoding="utf8")
-        archivo.write(texto)
-        return print("Registro satisfactorio")
+        self.archivo.write(texto)
 
     def cerrar_archivo(self):
-        archivo = open(self.ruta, self.permisos, encoding="utf8")
-        archivo.close()
-
-
-class Criptomendas(object):
-
-    def __init__(self, nombre, saldo, cotizacion):
-        self.nombre = nombre
-        self.saldo = saldo
-        self.cotizacion = cotizacion
+        self.archivo.close()
